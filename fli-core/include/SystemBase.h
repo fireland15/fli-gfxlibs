74689c03280fa8ad2800b9ee7b11c6b36e85e5a0
fireland15 <fireland15@my.whitworth.edu>
2016-03-31 00:12:58 -0700

    Made the worldstate a parameter to the update function. Allows changing the world state instantly during runtime.

--- fli-core/include/SystemBase.h
+++ fli-core/include/SystemBase.h
@@ -1,8 +1,10 @@
 #pragma once
 
+#include "WorldState.h"
+
 class SystemBase {
 public:
 	virtual void Start() = 0;
-	virtual void Update(double time) = 0;
+	virtual void Update(WorldState& worldState, double time) = 0;
 	virtual void Stop() = 0;
 };
